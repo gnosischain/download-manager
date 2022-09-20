f51c322ed63178f97077a794f9bd9d1524818b41
riccardo <riccardo.dantoni@gnosis.pm>
2022-09-20 20:05:16 +0200

    remove: unused

--- totalbar_posix.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows
-
-package main
-
-import "github.com/vbauerster/mpb/v8"
-
-func totalBarStyle() mpb.BarFillerBuilder {
-	return mpb.BarStyle().Lbound(" \x1b[36m").Padding("\x1b[0m-").Rbound("\x1b[0m ")
-}
--- totalbar_windows.go
+++
@@ -1,9 +0,0 @@
-//go:build windows
-
-package getparty
-
-import "github.com/vbauerster/mpb/v8"
-
-func totalBarStyle() mpb.BarFillerBuilder {
-	return mpb.BarStyle().Lbound(" ").Rbound(" ")
-}
